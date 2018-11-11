c14f25b65ae575cd0a5086ab1f5e828a1cac7a8e
Jaga My Priera <jagamypriera@gmail.com>
2018-11-11 15:46:27 +0700

    draw chart

---
+++ README.md
@@ -0,0 +1,7 @@
+#### Visualize .zsh_history
+##### How to run
+
+```javascript
+npm install
+npm run dev
+```
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,14 @@ import {
   TableHead,
   TableRow,
   TableCell,
-  TableBody
+  TableBody,
+  TextField,
+  Grid,
+  Toolbar,
+  Dialog,
+  AppBar,
+  Slide,
+  Typography
 } from "@material-ui/core"
 import * as R from "ramda"
 import { prop, pipe, map, chain } from "ramda"
@@ -17,19 +24,115 @@ import {
   VictoryContainer,
   Bar,
   VictoryLabel,
-  VictoryAxis
+  VictoryAxis,
+  VictoryZoomContainer,
+  VictoryLine,
+  VictoryTooltip,
+  VictoryVoronoiContainer
 } from "victory"
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close'
+const TBar = ({ data,width,title }) => {
+  return (
+    <Grid item xs={width}>
+      <Paper style={{ padding: 10 }}>
+        <VictoryLabel text={title} x={0} y={0} textAnchor="middle" angle={90}/>
+        <VictoryChart
+          
+          // containerComponent={<VictoryZoomContainer
+          //     zoomDimension="x"
+          //     minimumZoom={{x: 1/10000}}
+          //   />}
+        >
+          <VictoryAxis dependentAxis  />
+          <VictoryAxis
+            textAnchor="middle"
+            style={{
+              tickLabels: { fontSize: 10, angle: -0 }
+            }}
+          />
+          <VictoryBar
+            labelComponent={<VictoryLabel dy={-10} />}
+            labels={d => d.y}
+            alignment="middle"
+            style={{ data: { fill: "gold" }, labels: { fontSize: 10 } }}
+            dataComponent={<Bar  />}
+            data={data}
+          />
+        </VictoryChart>
+      </Paper>
+    </Grid>
+  );
+};
+const TLine = ({ data,width,title }) => {
+  return (
+    <Grid item xs={width}>
+      <Paper style={{ padding: 10 }}>
+        <VictoryLabel text={title} x={0} y={0} textAnchor="middle" angle={90}/>
+        <VictoryChart scale={{ x: "time" }} containerComponent={
+          <VictoryVoronoiContainer voronoiDimension="x"
+            labels={(d) => `Count: ${d.y}\nDate:${d.date}\nCommand:${JSON.stringify(d.command)}`}
+            labelComponent={<VictoryTooltip cornerRadius={0} flyoutStyle={{fill: "white"}}/>}
+          />
+        }>
+          <VictoryAxis dependentAxis  />
+          <VictoryAxis
+            textAnchor="middle"
+            style={{
+              tickLabels: { fontSize: 10, angle: -0 }
+            }}
+          />
+          <VictoryLine
+              data={data}
+              style={{
+                data: { stroke: "gold" }
+              }}
+              
+          />
+        </VictoryChart>
+      </Paper>
+    </Grid>
+  );
+};
+const Raw=({stateOpen,closeAction, raw})=>{
+  return(
+    <Dialog fullScreen
+          open={stateOpen}
+          onClose={closeAction}
+          TransitionComponent={Transition}
+        >
+    <IconButton color="inherit" onClick={closeAction} aria-label="Close">
+          <CloseIcon />
+    </IconButton>
+    <Typography style={{margin:20, padding:30}} >{JSON.stringify(raw, null, '\t')} </Typography>
+    {/* <TextField
+        style={{width:"100%", backgroundColor:'white'}}
+        label="Raw"
+        multiline
+        rowsMax="4"
+        value={JSON.stringify(raw)}
+        margin="normal"
+      /> */}
+  </Dialog>
+  )
+}
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
 class Index extends React.Component {
   constructor(props) {
     super(props)
     this.state = { 
+      open: false,
       loading: false ,
-      days :['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      days :['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      months: [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+      ]
     }
-  }
-  componentDidMount() {
-    this.setState({ lel: "lol" })
-    console.log(this.state.lel)
   }
   _onFilePicked(e) {
     this.setState({ loading: true })
@@ -38,22 +141,16 @@ class Index extends React.Component {
     fr.onload = () => {
       let data = this.parsing(fr.result)
       let plot = this.plot(data)
-      this.setState({ loading: false })
+      this.refs.file.value = ''
     }
+    fr.onerror=(e)=>console.log(e)
   }
   formatDate(date) {
-    var monthNames = [
-      "January", "February", "March",
-      "April", "May", "June", "July",
-      "August", "September", "October",
-      "November", "December"
-    ];
-  
-    var day = date.getDate();
-    var monthIndex = date.getMonth();
-    var year = date.getFullYear();
-  
-    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+    const {months}=this.state
+    let day = date.getDate()
+    let monthIndex = date.getMonth()
+    let year = date.getFullYear()
+    return day + ' ' + months[monthIndex] + ' ' + year
   }
   parsing(data) {
     let commands = data.trim().split(/:+\s+\d+:+\d+;/)
@@ -80,7 +177,7 @@ class Index extends React.Component {
         arguments: getArguments(commands,index),
         base_command: getBaseCommand(commands, index),
         timestamp: time,
-        hour: ("0" + date.getHours()).slice(-2)+':00',
+        hour: ("0" + date.getHours()).slice(-2),
         day:this.state.days[date.getDay()],
         date: this.formatDate(date),
       }
@@ -93,9 +190,10 @@ class Index extends React.Component {
   plot(param) {
     this.setState({ data: param },()=>{
       this.getFavoriteCommands(10)
-      this.getFavoriteHour()
-      this.getFavoriteDay()
-      this.getFavoriteDate()
+      this.getHourlyStats()
+      this.getDailyStats()
+      this.getDateStats()
+      this.setState({ loading: false })
     })
   }
   getFavoriteCommands(limit){
@@ -109,7 +207,15 @@ class Index extends React.Component {
       }
     })
     let sort=R.sortBy(d=>-d.count)
-    let favorite=R.compose(R.take(limit),sort,R.values,map,group)(data)
+    let mapToChart=R.map(d=>{
+      return {
+        x:d.base_command,
+        y:d.count
+      }
+    })
+    let favorite=R.compose(mapToChart,R.take(limit),sort,R.values,map,group)(data)
+    
+    this.setState({favorite})
     return favorite
   }
   /**
@@ -128,7 +234,7 @@ class Index extends React.Component {
     return favorite
   }
 
-  getFavoriteHour(){
+  getHourlyStats(){
     let {data}=this.state
     let group=R.groupBy(d=>d.hour)
     let map=R.map(d=>{
@@ -139,11 +245,18 @@ class Index extends React.Component {
       }
     })
     let sort=R.sortBy(d=>d.hour)
-    let favorite=R.compose(sort,R.values,map,group)(data)
-    return favorite
+    let mapToChart=R.map(d=>{
+      return {
+        x:d.hour,
+        y:d.count
+      }
+    })
+    let hourly=R.compose(mapToChart,sort,R.values,map,group)(data)
+    this.setState({hourly})
+    return hourly
   }
 
-  getFavoriteDay(){
+  getDailyStats(){
     let {data}=this.state
     let group=R.groupBy(d=>d.day)
     let map=R.map(d=>{
@@ -154,28 +267,56 @@ class Index extends React.Component {
       }
     })
     let sort=R.sortBy(d=>-d.count)
-    let favorite=R.compose(sort,R.values,map,group)(data)
-    return favorite
+    let mapToChart=R.map(d=>{
+      return {
+        x:d.day,
+        y:d.count
+      }
+    })
+    let daily=R.compose(mapToChart,sort,R.values,map,group)(data)
+    this.setState({daily})
+    return daily
   }
-  getFavoriteDate(){
+  getDateStats(){
     let {data}=this.state
     let group=R.groupBy(d=>d.date)
     let map=R.map(d=>{
       return {
         date:d[0].date,
         command:R.groupBy(d=>d.base_command,d),
+        count:d.length,
         timestamp:d[0].timestamp
       }
     })
     let sort=R.sortBy(d=>d.timestamp)
-    let favorite=R.compose(sort,R.values,map,group)(data)
-    return favorite
+    let mapToChart=R.map(d=>{
+      return {
+        x:new Date(d.timestamp),
+        y:d.count,
+        date:d.date,
+        command:d.command
+      }
+    })
+    let date=R.compose(mapToChart,sort,R.values,map,group)(data)
+    let d=R.compose(sort,R.values,map,group)(data)
+    console.log(d)
+    this.setState({date})
+    return date
   }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
   render() {
-    const { data } = this.state
+    const { data, hourly,daily,date,favorite,open } = this.state
     return (
-      <div style={{padding:50}}>
-        <Button
+      <div style={{ padding: 50 }}>
+      <Grid container spacing={24} style={{ marginTop: 20 }}>
+          <Grid item xs={2}><Button
           component="label"
           variant="contained"
           color="primary"
@@ -185,12 +326,21 @@ class Index extends React.Component {
           <input
             onChange={e => this._onFilePicked(e)}
             style={{ display: "none" }}
+            ref="file"
             type="file"
           />
-        </Button>
-        <Paper style={{marginTop:20}}>
-          
-        </Paper>
+        </Button></Grid>
+          <Grid item xs={2}><Button onClick={this.handleClickOpen}>Show raw text</Button></Grid>
+          <Grid item xs={8}></Grid>
+          <TBar data={hourly} width={6} title={"Hourly Chart (x: hour, y: commands count)"} />
+          <TBar data={daily} width={6} title={"Daily Chart (x: day, y: commands count)"} />
+          <TBar data={favorite} width={6} title={"Top 10 Commands (x: commands, y: count)"} />
+          <TLine data={date} width={6} title={"Day to day (x: commands, y: count)"} />
+          <Raw 
+          stateOpen={open} 
+          closeAction={()=>this.handleClose()}
+          raw={data}/>
+        </Grid>
       </div>
     )
   }
